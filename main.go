c1478943de03c0a3d79d2bc6f841501ddab64675
Enrique <edavalos@ucsc.edu>
2020-08-21 04:35:08 -0700

    added slow typing function

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	typeThis("Hello World")
 }
 
-
 func typeThis(text string) {
 	for _, c := range text {
 		fmt.Print(string(c))
-		time.Sleep(70 * time.Millisecond)
+		if string(c) == string(' ') {
+			time.Sleep(30 * time.Millisecond)
+		} else {
+			time.Sleep(85 * time.Millisecond)
+		}
 	}
 }
