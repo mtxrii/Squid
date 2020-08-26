63e02a1997125cb54ddc564ed7e129300df39555
Enrique <edavalos@ucsc.edu>
2020-08-26 04:50:20 -0700

    established test environment

--- glyphs.go
+++ glyphs.go
@@ -1,54 +1,63 @@
 package main
 
-var charGlyphs = map[rune]string{
-	'a': "║╝",
-	'b': "╚╗",
-	'c': "╔╝",
-	'd': "╔╗",
-	'e': "║╗",
-	'f': "╔═",
-	'g': "═╗",
-	'h': "╚╝",
-	'i': "║║",
-	'j': "═╝",
-	'k': "══",
-	'l': "╚═",
-	'm': "╗╝",
-	'n': "╝╝",
-	'o': "╔║",
-	'p': "╝╔",
-	'q': "╝╗",
-	'r': "╚╚",
-	's': "═╚",
-	't': "╔╔",
-	'u': "╚║",
-	'v': "═╔",
-	'w': "╗╗",
-	'x': "╗═",
-	'y': "═║",
-	'z': "╗╚",
+func GetCharGlyphs() map[rune]string {
+	var charGlyphs = map[rune]string{
+		'a': "║╝",
+		'b': "╚╗",
+		'c': "╔╝",
+		'd': "╔╗",
+		'e': "║╗",
+		'f': "╔═",
+		'g': "═╗",
+		'h': "╚╝",
+		'i': "║║",
+		'j': "═╝",
+		'k': "══",
+		'l': "╚═",
+		'm': "╗╝",
+		'n': "╝╝",
+		'o': "╔║",
+		'p': "╝╔",
+		'q': "╝╗",
+		'r': "╚╚",
+		's': "═╚",
+		't': "╔╔",
+		'u': "╚║",
+		'v': "═╔",
+		'w': "╗╗",
+		'x': "╗═",
+		'y': "═║",
+		'z': "╗╚",
+	}
+	return charGlyphs
 }
 
-var numberGlyphs = map[rune]string{
-	'1': "(ƞ",
-	'2': "@#",
-	'3': "Ʃ^",
-	'4': "*¢",
-	'5': "(Ʈ",
-	'6': "+Ĳ",
-	'7': "ƴƴ",
-	'8': "_Ɨ",
-	'9': "Ɩ)",
-	'0': "Ɖ)",
+func GetNumberGlyphs() map[rune]string {
+	var numberGlyphs = map[rune]string{
+		'1': "(ƞ",
+		'2': "@#",
+		'3': "Ʃ^",
+		'4': "*¢",
+		'5': "(Ʈ",
+		'6': "+Ĳ",
+		'7': "ƴƴ",
+		'8': "_Ɨ",
+		'9': "Ɩ)",
+		'0': "Ɖ)",
+	}
+	return numberGlyphs
 }
 
-var specialGlyphs = map[rune]string{
-	'.': "Ͳ╗",
-	',': "═Ȝ",
-	'"': ">(",
-	'-': "Ϟ╝",
-	';': "╚$",
-	':': "ī╔",
+func GetSpecialGlyphs() map[rune]string {
+	var specialGlyphs = map[rune]string{
+		'.': "Ͳ╗",
+		',': "═Ȝ",
+		'"': ">(",
+		'-': "Ϟ╝",
+		';': "╚$",
+		':': "ī╔",
+	}
+	return specialGlyphs
 }
 
 /*
--- main.go
+++ main.go
@@ -22,12 +22,27 @@ func typeThis(text string) {
 }
 
 func encode(text string) string {
+	var charGlyphs = GetCharGlyphs()
+	var numberGlyphs = GetNumberGlyphs()
+	var specialGlyphs = GetSpecialGlyphs()
+
 	var result string
 	for _, c := range text {
 		if c == ' ' {
 			result += string(' ')
 		} else if unicode.IsDigit(c) {
 			result += numberGlyphs[c]
+		} else if unicode.IsUpper(c) {
+			result += "$" + charGlyphs[unicode.ToLower(c)]
+		} else if unicode.IsLower(c) {
+			result += charGlyphs[c]
+		} else {
+			key, exists := specialGlyphs[c]
+			if exists {
+				result += key
+			} else {
+				result += string(c)
+			}
 		}
 	}
 	return result
